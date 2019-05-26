3e9c72fae30c5411a9f59afa1201fead19d29290
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2019-05-26 15:33:48 +0200

    Various changes.

--- src/Module/MemberMap.php
+++ src/Module/MemberMap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace ErdmannFreunde\ContaoMemberMapBundle\Module;
 
-use Contao\{BackendTemplate, Module, StringUtil, System};
+use Contao\{BackendTemplate, Config, Module, PageModel, StringUtil, System};
 use Doctrine\DBAL\Connection;
 use Patchwork\Utf8;
 
@@ -37,7 +37,7 @@ class MemberMap extends Module
         $connection = System::getContainer()->get('database_connection');
 
         $statement = $connection->createQueryBuilder()
-            ->select('city', 'geo_longitude', 'geo_latitude', 'm2g.group_id')
+            ->select('*', 'm2g.group_id')
             ->from('tl_member', 'm')
             ->innerJoin('m', 'tl_member_to_group', 'm2g', 'm2g.member_id=m.id')
             ->where('m2g.group_id IN (:groups)')
@@ -50,11 +50,36 @@ class MemberMap extends Module
         }
 
         $places = [];
-        while ($row = $statement->fetch(\PDO::FETCH_OBJ)) {
-            $places[$row->group_id][] = [$row->city, $row->geo_latitude, $row->geo_longitude];
+        while ($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
+            try {
+                $title = StringUtil::parseSimpleTokens($this->mapPlaceTitle, $row) ?: $row['city'];
+            } catch (\Exception $e) {
+                $title = $row['city'];
+            }
+
+            $places[$row->group_id][] = [$title, $row['geo_latitude'], $row['geo_longitude']];
         }
 
         $this->Template->placesJson = json_encode($places);
-        $this->Template->apiKey     = 'AIzaSyBWhf5ItmL745ISjJ2E0iXSzEoPJm1yp0A';
+        $this->Template->apiKey     = $this->getApiKey();
+    }
+
+    private function getApiKey(): string
+    {
+        global $objPage;
+
+        if (null !== ($rootPage = PageModel::findByPk($objPage->rootId))) {
+            $key = $rootPage->dlh_googlemaps_apikey;
+        }
+
+        if (!$key) {
+            $key = Config::get('dlh_googlemaps_apikey');
+        }
+
+        if (!$key) {
+            $key = Config::get('googleMapsApiKey');
+        }
+
+        return (string) $key;
     }
 }
--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -5,7 +5,7 @@
  * Palettes
  */
 $GLOBALS['TL_DCA']['tl_module']['palettes']['member_map'] =
-    '{title_legend},name,headline,type;{config_legend},mapMemberGroups;{template_legend:hide},navigationTpl,customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+    '{title_legend},name,headline,type;{config_legend},mapMemberGroups,mapPlaceTitle;{template_legend:hide},navigationTpl,customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
 
 /**
@@ -18,4 +18,12 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['mapMemberGroups'] = [
     'eval'       => ['tl_class' => 'w50', 'multiple' => true, 'chosen' => true],
     'foreignKey' => 'tl_member_group.name',
     'sql'        => 'blob NULL'
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['mapPlaceTitle'] = [
+    'label'      => &$GLOBALS['TL_LANG']['tl_module']['mapPlaceTitle'],
+    'exclude'    => true,
+    'inputType'  => 'text',
+    'eval'       => ['tl_class' => 'w50', 'chosen' => true],
+    'sql'        => "varchar(255) NOT NULL default ''"
 ];
